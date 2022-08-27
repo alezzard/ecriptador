15655c9ead0b62380efb0625caeba63db4862403
Ale-sentey <gabriel.alegodoy92@gmail.com>
2022-08-27 01:59:17 -0300

    encrypt y decrypt funcionan correctamente

--- script.js
+++ script.js
@@ -12,60 +12,72 @@
 */
 
 const textInput = document.querySelector(".textInput");
-let textOutput = document.querySelector(".textOutput");
+const textOutput = document.querySelector(".textOutput");
+const message = document.querySelector(".message");
+const buttonCopy = document.querySelector(".copy")
 const codigo = [["e", "enter"],
-["i", "imes"],
-["a", "ai"],
-["o", "ober"],
-["u", "ufat"],];
+                ["i", "imes"],
+                ["a", "ai"],
+                ["o", "ober"],
+                ["u", "ufat"],];
 
 function btnEncrypt(){
     const textEncrypted = encrypt(textInput.value);
     textOutput.value = textEncrypted;
-    textOutput.style.backgroundImage = "none";
+    console.log("textEncrypted: "+typeof textEncrypted.value)
+    // textOutput.style.backgroundImage = "none";
+    message.style.display = "none";
+    buttonCopy.style.display = "flex";
     textInput.value = "";    
 }
 
 function btnDecrypt(){
     const textDencrypted = decrypt(textInput.value);
     textOutput.value = textDencrypted;
-    textOutput.style.backgroundImage = "url(assets/Muñeco.png)";
+    message.style.display = "none";
+    textOutput.style.backgroundImage = "none";
     textInput.value = ""; 
 }
 
 function copy() {
     textOutput.select();
     navigator.clipboard.writeText(textOutput.value)
+    console.log("textOutput.value : "+textOutput.value)
     textInput.value = textOutput.value;
     textOutput.value = "";
-    textOutput.style.backgroundImage = "url(assets/Muñeco.png)";
+    message.style.display = "block";
+    // textOutput.style.backgroundImage = "url(assets/Muñeco.png)";
 }
 
 function encrypt(textInput) {
-    textOutput = textInput
-        .toLowerCase()
-        .replace(/[^a-z ]/g, "");
-        console.log("antes del for : textOutput :"+textOutput+" textInput: "+textInput)
-    for (let i = 0; i < codigo.length; i++) {
-        console.log("vuelta nº"+i+" del for : textOutput :"+textOutput+" textInput: "+textInput)
-        if (textOutput.includes(codigo[i][0])) {
-            console.log("if")
-            textOutput = textOutput.replaceAll
-                (codigo[i][0], codigo[i][1]);
+    if(textInput){
+        let textOutput = textInput
+                            .toLowerCase()
+                            .replace(/[^a-z ]/g, "");
+        for (let i = 0; i < codigo.length; i++) {
+            console.log("vuelta nº"+i+" del for : textInput: "+textInput+" textOutput :"+textOutput)
+            if (textOutput.includes(codigo[i][0])) {
+                textOutput = textOutput
+                .replaceAll(codigo[i][0], codigo[i][1]);
+            }
         }
+        return textOutput;
     }
-    return textOutput;
+    return "";
 }
 
 function decrypt(textInput) {
-    textOutput = textInput
-        .toLowerCase()
-        .replace(/[^a-z ]/g, "");
-    for (let i = 0; i < codigo.length; i++) {
-        if (textOutput.includes(codigo[i][1])) {
-            textOutput = textOutput.replaceAll
-                (codigo[i][1], codigo[i][0]);
+    if(textInput){
+        let textOutput = textInput
+                            .toLowerCase()
+                            .replace(/[^a-z ]/g, "");
+        for (let i = 0; i < codigo.length; i++) {
+            if (textOutput.includes(codigo[i][1])) {
+                textOutput = textOutput
+                                .replaceAll(codigo[i][1], codigo[i][0]);
+            }
         }
+        return textOutput;
     }
-    return textOutput;
+    return "";
 }
